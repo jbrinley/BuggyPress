b0af80985d95ad018a53d66df6c3a886afd041a7
Jonathan Brinley <jbrinley@flightless.us>
2012-07-28 16:43:33 -0400

    set project issues rewrite rule

--- classes/BuggyPress.php
+++ classes/BuggyPress.php
@@ -99,6 +99,7 @@ class BuggyPress {
 			'BuggyPress_Menus',
 			'BuggyPress_CommentForms',
 			'BuggyPress_NewIssuePage',
+			'BuggyPress_IssueQueryPage',
 		);
 		foreach ( $to_init as $pt ) {
 			add_action(self::PLUGIN_INIT_HOOK, array($pt, 'init'));
